89a00b7b53fd92fdd3b39a76a4ba8ad14b92f6d6
endungu <eric.ndungu16@gmail.com>
2025-09-19 17:59:05 +0300

    =counter

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,51 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnDestroy, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DecimalPipe, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NgOptimizedImage,
+    DecimalPipe
+  ],
+  standalone:true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('ann-weds-eric');
+export class App implements OnDestroy{
+
+  days = signal(0);
+  hours = signal(0);
+  minutes = signal(0);
+  seconds = signal(0);
+  // Countdown Timer
+  targetDate = new Date("2025-11-07T23:59:59").getTime();
+
+  updateCountdown() {
+    const now = new Date().getTime();
+    const distance = this.targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    this.days.set(days);
+    this.hours.set(hours);
+    this.minutes.set(minutes);
+    this.seconds.set(seconds);
+  }
+   intervalId!: ReturnType<typeof setInterval>;
+  constructor() {
+    // Start interval on init
+    this.intervalId = setInterval(() => {
+      this.updateCountdown()
+    }, 1000);
+
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId); // ✅ cleanup
+  }
 }
+// our preference is cash but we are open to other gifts
